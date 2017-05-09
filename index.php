daeac9cc37423bb1ec8b57e16dbf7496d7094cab
jmronco <cetecom@LC08L5714>
2017-05-09 10:22:51 -0300

    aswdasdsad

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Pirate</title>
     </head>
     <body>
         <?php
