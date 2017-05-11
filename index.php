cb188983b33ce7e6486b786c4d584f7e40bf12f3
jmronco <jmronco@miuandes.cl>
2017-05-11 12:00:54 -0300

    Update index.php
    11-05v2.

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 include ('lib/Producto.php');
 include ('lib/ConsultaProductos.php');
+
+
 $oProducto1=new Producto();
 $oProducto1->nombre="Nuevo Producto";
 $oProducto1->codigo="001";
+
+
 $oProducto2=new Producto("Nuevo Producto 002",100000,"002");
 $oProducto3=new Producto("Nuevo Producto 003",250000,"003");
+
+
 $aProductos[0]=$oProducto1;
 $aProductos[1]=$oProducto2;
 $aProductos[2]=$oProducto3;
