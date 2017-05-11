7012a23a1376f51e3ca1176fd6b5550c8bb4ebb2
jmronco <jmronco@miuandes.cl>
2017-05-11 12:00:18 -0300

    Update index.php
    11-05

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
 <?php
 include ('lib/Producto.php');
-
-$oProducto1 = new Producto();
+include ('lib/ConsultaProductos.php');
+$oProducto1=new Producto();
 $oProducto1->nombre="Nuevo Producto";
 $oProducto1->codigo="001";
+$oProducto2=new Producto("Nuevo Producto 002",100000,"002");
+$oProducto3=new Producto("Nuevo Producto 003",250000,"003");
+$aProductos[0]=$oProducto1;
+$aProductos[1]=$oProducto2;
+$aProductos[2]=$oProducto3;
 
-
-$oProducto2 = new Producto("Nuevo Producto 002", 100000, "002");
-$oProducto3 = new Producto("Nuevo Producto 003", 250000, "003");
-
-
-/*$oProducto1->nombre="Nuevo Producto";
-$oProducto1->codigo="001";
-$oProducto1->nombre="Nuevo Producto 002";
-$oProducto1->codigo="002";
-$oProducto1->nombre="Nuevo Producto 002";
-$oProducto1->codigo="003";
-gvhgv*/
-
-$aProductos[0] = $oProducto1;
-$aProductos[1] = $oProducto2;
-$aProductos[2] = $oProducto3;
 
 ?>
 <!DOCTYPE html>
